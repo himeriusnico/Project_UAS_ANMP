6a82dd4cc4062e31100805bcb514ac3e9fa3dec4
himeriusnico <himeriusnico03@gmail.com>
2025-07-03 17:45:04 +0700

    AKHIRNYA
    SIGNINSIGNUP BERJALAN

--- app/src/main/java/com/ubaya/project_uas/view/MainActivity.kt
+++ app/src/main/java/com/ubaya/project_uas/view/MainActivity.kt
@@ -2,17 +2,29 @@ package com.ubaya.project_uas.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.ubaya.project_uas.R
 import com.ubaya.project_uas.databinding.ActivityMainBinding
 import com.ubaya.project_uas.view.SignInFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(binding.mainContainer.id) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
